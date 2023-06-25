05b629446cb65ab5fcf5d957aa121dfc0e35bd8b
H.D.Hien <huynhduyhien1997@gmail.com>
2023-06-25 20:48:40 +0700

    second commit

---
+++ scripts/initdb.sql
@@ -0,0 +1,17 @@
+-- DROP DATABASE IF EXISTS `ciapp`;
+-- CREATE DATABASE IF NOT EXISTS `ciapp`;
+-- USE `ciapp`;
+
+-- DROP TABLE IF EXISTS `user`;
+-- CREATE TABLE IF NOT EXISTS `user` (
+--   `id` int(11) NOT NULL,
+--   `name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+--   `email` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+--   `phone` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+--   `password` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+--   `timestamp` timestamp(6) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`),
+--   UNIQUE KEY `id_UNIQUE` (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE DATABASE ciapp;
